0ae10b976601bb263a9936a6bb65b8d7ef1fb336
ksebin <ksebin96@gmail.com>
2023-06-20 13:48:20 +0900

    2023-06-20

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const express = require('express');
-const app = express()
-const port = 5100
-const { default: mongoose } = require('mongoose');
+const express = require("express");
+const app = express();
+const port = 5100;
+const { default: mongoose } = require("mongoose");
+const { User } = require('./models/User');
+const config = require('./config/key');
+
+app.use(express.json());
 
 
-mongoose.connect('mongodb+srv://ksebin96:tpqls0417!@cluster0.34voyd1.mongodb.net').then(() => {console.log('MongoDB Connected')})
- .catch(err => console.log(err))
+mongoose
+  .connect(config.mongoURI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+app.post("/register", (req, res) => {
+  // 회원 가입할 떄 필요한 정보들을 클라이언트에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다
+  const user = new User(req.body);
+
+  user.save()
+  .then((userInfo) => res.status(200).json({success: true}))
+  .catch((err) => res.json({success: false, err}));
+
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
-
